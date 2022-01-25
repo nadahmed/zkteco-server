deb772fd4a498b90913e5e4f4198ed1bcf63f6d1
Suvra <kar.suvra@gmail.com>
2022-01-25 17:26:40 +0600

    sh file changed

--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
-if [[ ! -e /code/test.db ]]; then
+#!/bin/bash
+
+FILE=/code/test.db
+if [ -f "$FILE" ]; then
+    echo "File found. Do something meaningful here"
+else
     echo "File does not exist in Bash"
     echo "Running database migrations..."
-    alembic upgrade head
-else
-    echo "File found. Do something meaningful here"
+    alembic upgrade head   
+
 fi
+echo "Running server now"
 
 uvicorn main:app --host 0.0.0.0 --port 8000
+
+exit 0
